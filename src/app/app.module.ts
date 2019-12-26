7f31ae56f3f90d6f3a2727ffa0c69e517e41d000
azar103 <azzarroukanis@gmail.com>
2019-12-26 14:10:35 +0100

    add recipe component -display list of recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListRecipesComponent } from './list-recipes/list-recipes.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListRecipesComponent
+    ListRecipesComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/list-recipes/list-recipes.component.ts
+++ src/app/list-recipes/list-recipes.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-recipes',
   templateUrl: './list-recipes.component.html',
-  styleUrls: ['./list-recipes.component.sass']
+  styleUrls: ['./list-recipes.component.css']
 })
 export class ListRecipesComponent implements OnInit {
-  title = 'mon premier component';
+  nameRecipeOne = 'Pumpkin Pie';
+  urlRecipeOne = 'https://assets.afcdn.com/recipe/20130122/48702_w420h344c1cx638cy864.jpg';
+  nameRecipeTwo = 'Spaghetti';
+  urlRecipeTwo = 'https://static.cuisineaz.com/240x192/i2142-spaghetti-a-la-sauce-tomate.jpg';
+  nameRecipeThree = 'Onion Pie';
+  urlRecipeThree = 'https://images.media-allrecipes.com/userphotos/560x315/6753640.jpg';
   constructor() { }
 
   ngOnInit() {
